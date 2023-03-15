d58969c4ee307ca2ad00c605891a11399861fb29
cheeky sim <48182295+cheekysim@users.noreply.github.com>
2023-03-15 08:19:02 +0000

    Initial Commit

---
+++ Mnist/main.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+# Command to install tensorflow_datasets: pip install tensorflow-datasets
+# Command to install tensorflow: pip install tensorflow
+
+def main():
+    print("Hello World")
+
+if __name__ == '__main__':
+    main()
